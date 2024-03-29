8cf325832f938b29451f9e10f08652d5793d70af
sunepoulsen <sunepoulsen@gmail.com>
2024-03-29 20:27:58 +0100

    TESENTLABS-7: Add use of certificates for https.

--- tel-testdata/tel-testdata-module/src/main/java/dk/sunepoulsen/tel/testdata/module/service/Application.java
+++ tel-testdata/tel-testdata-module/src/main/java/dk/sunepoulsen/tel/testdata/module/service/Application.java
@@ -3,6 +3,7 @@ package dk.sunepoulsen.tel.testdata.module.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @EnableWebMvc
 @EnableAsync
+@EnableWebSecurity
 @SpringBootApplication( scanBasePackages = {
     "dk.sunepoulsen.tes",
     "dk.sunepoulsen.tel"
