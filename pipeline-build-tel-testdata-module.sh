8a6cd9b7cb740f1755424fac1665b3c5f23c8e17
sunepoulsen <sunepoulsen@gmail.com>
2024-04-05 20:29:05 +0200

    TESENTLABS-27: Add `tel-testdata:tel-testdata-integrations` to the `tel-testdata` build step.

--- pipeline-build-tel-testdata-module.sh
+++ pipeline-build-tel-testdata-module.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Building TEL TestData Module"
+./gradlew :tel-testdata:tel-testdata-integrations:build --stacktrace || exit
 ./gradlew :tel-testdata:tel-testdata-module:build --stacktrace || exit
