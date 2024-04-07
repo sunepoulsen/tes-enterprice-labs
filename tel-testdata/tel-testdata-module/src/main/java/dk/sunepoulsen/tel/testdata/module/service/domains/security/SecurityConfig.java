941a30917d1db73d0b24b824c267e0ae7f3f5366
sunepoulsen <sunepoulsen@gmail.com>
2024-04-07 17:59:10 +0200

    TESENTLABS-27: Disabled CSRF support.

--- tel-testdata/tel-testdata-module/src/main/java/dk/sunepoulsen/tel/testdata/module/service/domains/security/SecurityConfig.java
+++ tel-testdata/tel-testdata-module/src/main/java/dk/sunepoulsen/tel/testdata/module/service/domains/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package dk.sunepoulsen.tel.testdata.module.service.domains.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -17,6 +18,8 @@ public class SecurityConfig {
             authorizationManagerRequestMatcherRegistry
                 .requestMatchers(mvcMatcherBuilder.pattern("/**"))
                 .permitAll());
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
