2060c69d77c1a316608ac198c8402b1030426a6d
sunepoulsen <sunepoulsen@gmail.com>
2024-04-08 02:03:24 +0200

    TESENTLABS-24: Include building the JavaDoc in the build pipeline.

--- pipeline-build-tel-testdata-module.sh
+++ pipeline-build-tel-testdata-module.sh
@@ -2,4 +2,6 @@
 
 echo "Building TEL TestData Module"
 ./gradlew :tel-testdata:tel-testdata-integrations:build --stacktrace || exit
+./gradlew :tel-testdata:tel-testdata-integrations:JavaDoc --stacktrace || exit
 ./gradlew :tel-testdata:tel-testdata-module:build --stacktrace || exit
+./gradlew :tel-testdata:tel-testdata-module:JavaDoc --stacktrace || exit
